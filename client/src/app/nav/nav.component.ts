806d46e0ffd8d44faf266b353918a9041e644b22
ariful-2019331062 <arifulfarhad300@gmail.com>
2025-04-03 19:15:30 +0600

    end of section 6

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -1,19 +1,28 @@
 import { Component, inject } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AccountService } from '../_services/account.service';
-import { catchError } from 'rxjs';
 import { CommonModule, NgIf } from '@angular/common';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { Router, RouterLink, RouterLinkActive } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-nav',
-  imports: [FormsModule, CommonModule, BsDropdownModule],
+  imports: [
+    RouterLink,
+    FormsModule,
+    CommonModule,
+    BsDropdownModule,
+    RouterLinkActive,
+  ],
   templateUrl: './nav.component.html',
   styleUrl: './nav.component.css',
 })
 export class NavComponent {
   accountService = inject(AccountService);
+  private router = inject(Router);
+  private toaster = inject(ToastrService);
 
   model: any = {};
 
@@ -22,9 +31,10 @@ export class NavComponent {
     // console.log(this.model);
     this.accountService.login(this.model).subscribe({
       next: (response) => {
-        console.log(response);
+        this.router.navigateByUrl('/members');
       },
-      error: (error) => console.log(error),
+      // error: (error) => console.log(error),
+      error: (error) => this.toaster.error(error.error),
       complete() {
         console.log('request has been complete');
       },
@@ -32,5 +42,6 @@ export class NavComponent {
   }
   logout() {
     this.accountService.logout();
+    this.router.navigateByUrl('/');
   }
 }
